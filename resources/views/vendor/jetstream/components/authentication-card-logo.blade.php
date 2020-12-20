6bbd88bd1fc46f78d829eee5db0d12f4a17ba992
adiartawibawa <surat.buat.adi@gmail.com>
2020-12-20 22:19:35 +0800

    config jetstream

---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,8 @@
+<a href="/">
+    <svg class="w-16 h-16" version="1.2" baseProfile="tiny" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+	 x="0px" y="0px" viewBox="0 0 292.7 250.7" xml:space="preserve" {{ $attributes }}>
+    <path fill="none" stroke="#FC5948" stroke-width="20" stroke-miterlimit="10" d="M90.6,166.2l39.5,74.5H16.5
+        C42,192.1,94,160,149.5,161.1c53.5,1,102.3,31.8,126.8,79.6H196l-87.9-153l39.6-67.8l76,130"/>
+    </svg>
+
+</a>
